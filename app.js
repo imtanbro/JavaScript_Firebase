7888428e7b49258883b515754da64e8e278aaeb0
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2020-12-21 22:35:30 +0530

    Successfully fetched Data from FireStore

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+const cafeList = document.querySelector("#cafe-list");
+
+//Create element and render cafe
+
+function renderCafe(doc){
+    let li = document.createElement("li");
+    let name = document.createElement("span");
+    let city = document.createElement("span");
+
+    li.setAttribute("data-id", doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+    console.log(li);
+
+}
+
+
+
+
 db.collection('cafes').get().then((snapshot) => {
     console.log(snapshot.docs);
+    console.log("---------------snapshot.docs----------------");
+    snapshot.docs.forEach(doc => {      
+        console.log(doc.data());
+        renderCafe(doc);
+    })
 })
